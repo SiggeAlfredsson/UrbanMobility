5283f1cdbc09001660f3e6aaf7116bc0bec632fc
Sigge <saorust@hotmail.com>
2023-09-28 17:02:51 +0200

    added empty classes with comments

---
+++ src/main/java/com/siggebig/demo/controllers/BookingController.java
@@ -0,0 +1,19 @@
+package com.siggebig.demo.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookingController {
+
+    // make a booking to a trip, includes making payment
+
+    // read info about a users bookings
+
+    // delete a bookings, removes payment
+
+
+
+    // cant really update a booking no? just delete and make new in that case
+
+}
---
+++ src/main/java/com/siggebig/demo/controllers/TripController.java
@@ -0,0 +1,17 @@
+package com.siggebig.demo.controllers;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TripController {
+
+    // create a trip
+
+    // delete a trip
+
+    // edit a trip
+
+    // get a trips info
+
+}
